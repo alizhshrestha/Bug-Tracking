5a39f427039e9d78e072a6e787ec9882f2d99d09
unknown <alizhshrestha@gmail.com>
2018-11-12 23:36:50 +0545

    created login form and connected to mysql database

---
+++ BugTracking/BugTracking/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BugTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.login_panel = new System.Windows.Forms.Panel();
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.login_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            resources.ApplyResources(this.btn_login, "btn_login");
+            this.btn_login.Name = "btn_login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // btn_exit
+            // 
+            resources.ApplyResources(this.btn_exit, "btn_exit");
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // txt_username
+            // 
+            resources.ApplyResources(this.txt_username, "txt_username");
+            this.txt_username.Name = "txt_username";
+            // 
+            // txt_password
+            // 
+            resources.ApplyResources(this.txt_password, "txt_password");
+            this.txt_password.Name = "txt_password";
+            // 
+            // login_panel
+            // 
+            this.login_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.login_panel.Controls.Add(this.lbl_login);
+            resources.ApplyResources(this.login_panel, "login_panel");
+            this.login_panel.Name = "login_panel";
+            // 
+            // lbl_login
+            // 
+            resources.ApplyResources(this.lbl_login, "lbl_login");
+            this.lbl_login.Name = "lbl_login";
+            // 
+            // lbl_username
+            // 
+            resources.ApplyResources(this.lbl_username, "lbl_username");
+            this.lbl_username.Name = "lbl_username";
+            // 
+            // lbl_password
+            // 
+            resources.ApplyResources(this.lbl_password, "lbl_password");
+            this.lbl_password.Name = "lbl_password";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_username);
+            this.Controls.Add(this.login_panel);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form1";
+            this.login_panel.ResumeLayout(false);
+            this.login_panel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Panel login_panel;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_login;
+    }
+}
+
---
+++ BugTracking/BugTracking/Form1.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracking
+{
+    public partial class Form1 : Form
+    {
+        //SqlConnection mySqlConnection;
+
+        public Form1()
+        {
+            /*InitializeComponent();
+            String[] myData = new string[100];
+            
+
+            mySqlConnection = new SqlConnection("server=Localhost;user id=root;database=bug_tracking");
+
+            String selcmd = "SELECT USERNAME FROM LOGIN";
+
+            SqlCommand mySqlCommand = new SqlCommand(selcmd, mySqlConnection);
+
+            mySqlConnection.Open();
+
+            SqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+            int i = 0;
+            while (mySqlDataReader.Read())
+            {
+                Console.WriteLine(mySqlDataReader["USERNAME"]); //reads a line of the query result at a time
+                myData[i++] = (String)mySqlDataReader["name"]; // store in an array too for use later
+
+            }
+
+            for(int j = 0; j<1; j++)
+            {
+                Console.WriteLine("Your username is: " + myData[j]);
+            }
+            */
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+
+            Main main = new Main();
+            main.Show();
+            this.Hide();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
